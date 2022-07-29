4605b6fc6abffc499b66245400613b615dd9d8a3
Rajiv Seelam <rajiv@betalectic.com>
2022-07-29 11:53:16 +0530

    Remove nav

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -99,9 +99,9 @@ function Header({ navigation }) {
           : 'dark:bg-transparent'
       )}
     >
-      <div className="mr-0 flex lg:hidden">
-        {/* <MobileNavigation navigation={navigation} /> */}
-      </div>
+      {/* <div className="mr-6 flex lg:hidden">
+        <MobileNavigation navigation={navigation} />
+      </div> */}
       <div className="relative flex flex-grow basis-0 items-center">
         <Link href="/" aria-label="Home page">
           <h1 className="text-xl font-bold tracking-tight text-black dark:text-white md:text-xl">
@@ -197,6 +197,17 @@ export function Layout({ children, title, tableOfContents }) {
       {/* {isHomePage && <Hero />} */}
 
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
+        {/* <div className="hidden lg:relative lg:block lg:flex-none">
+          <div className="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
+          <div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto py-16 pl-0.5">
+            <div className="absolute top-16 bottom-0 right-0 hidden h-12 w-px bg-gradient-to-t from-slate-800 dark:block" />
+            <div className="absolute top-28 bottom-0 right-0 hidden w-px bg-slate-800 dark:block" />
+            <Navigation
+              navigation={navigation}
+              className="w-64 pr-8 xl:w-72 xl:pr-16"
+            />
+          </div>
+        </div> */}
         <div className="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16">
           <article>
             {(title || section) && (
